9882c812c5da196ad37c35bc32cbdf547ad595f4
jeancosta66 <costajean1005@gmail.com>
2025-03-21 18:08:36 -0300

    Create ex_009.py

---
+++ ex_009.py
@@ -0,0 +1,9 @@
+#escreva um software que some dois numeros
+
+print("digiter um valor: ")
+numero1 = int(input())
+print("digiter outro valor: ")
+numero2 = int(input())
+
+soma = numero1 + numero2
+print(f'a soma {numero1} e {numero2} é {soma}')
