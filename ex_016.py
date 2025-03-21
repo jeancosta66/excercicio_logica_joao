5fe93c24b3f52ef4bad8ebe27d9173e071c3e427
jeancosta66 <costajean1005@gmail.com>
2025-03-21 08:44:43 -0300

    Update ex_016.py

--- ex_016.py
+++ ex_016.py
@@ -1,11 +1,13 @@
-# As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00 se forem compradas pelo menos 12. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o custo total da compra
-
-print("Digite quantas maçãs você desejar: ")
+#As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00 se forem compradas pelo menos 12. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o custo total da compra.
+print("Digite a quantidade de maça para ser comprada: ")
 num_macas = int(input())
-valor = 0 
-if num_macas <= 12 :
-    valor = 1.30
-    print("Valor de cada maça é R$1,00")
+desconto = 1.00
+normal = 1.80
+if num_macas < 12:
+    print("você irá pagar pelo valor 1,00")
+    custo_total = num_macas * desconto
 else:
-    valor = 1.00
-    print("Valor de cada maçã é R$1,30")
+    print("Você irá pagar pelo valor 1,80")
+    custo_total = num_macas * normal
+
+print(f"preço total você irá pagar é: R$ {custo_total:.2f}")
