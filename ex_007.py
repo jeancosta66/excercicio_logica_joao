0a5a8784d5f23141b28e83e802213ac9b74ed1e8
jeancosta66 <costajean1005@gmail.com>
2025-03-21 17:54:15 -0300

    Create ex_007.py

---
+++ ex_007.py
@@ -0,0 +1,13 @@
+#Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+
+print("         x")
+print("        xxx")
+print("       xxxxx")
+print("      xxxxxxx")
+print("     xxxxxxxxx")
+print("   xxxxxxxxxxxx")
+print("  xxxxxxxxxxxxxx")
+print(" xxxxxxxxxxxxxxxx")
+print("         xx")
+print("         xx")
+print("        xxxx")
