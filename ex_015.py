bf180e8079e3eb2d222093d8433d899f400f750a
jeancosta66 <costajean1005@gmail.com>
2025-03-21 07:08:40 -0300

    Create ex_015.py

---
+++ ex_015.py
@@ -0,0 +1,5 @@
+# Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo)
+if (-12):
+    print("Esse valor é negativo:",-12)
+else: 
+    print("Esse valor é positivo")
