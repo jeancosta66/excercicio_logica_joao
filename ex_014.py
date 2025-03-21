11c8132a87a527f9a06eb747a7a8f6bd48bba7a6
jeancosta66 <costajean1005@gmail.com>
2025-03-21 07:12:18 -0300

    Create ex_014.py

---
+++ ex_014.py
@@ -0,0 +1,6 @@
+#Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+
+if(10 < 12):
+    print("É maior que 10")
+else:
+    print("Não é maior que 10")
