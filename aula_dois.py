a512b91b41799b5574bcfcdaccc7c5609f9e7699
jeancosta66 <costajean1005@gmail.com>
2025-03-21 07:20:43 -0300

    Create aula_dois.py

---
+++ aula_dois.py
@@ -0,0 +1,8 @@
+#Faça um algoritmo eu pergunte a idade de uma pessoa e informe se pode entrar na festa (maior de 18 anos ), caso contrário, informe pra pessoa que ela deve esperar alguns anos 
+
+print ("qual é a idade")
+idade = int(input())
+if  idade<18:
+    print ("você não pode entrar na festa")
+else:
+    print ("você pode entrar na festa")
