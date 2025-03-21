66bd1b6864e3ea7812880eceb5976f903a309b28
jeancosta66 <costajean1005@gmail.com>
2025-03-21 09:03:51 -0300

    Update ex_014.py

--- ex_014.py
+++ ex_014.py
@@ -1,6 +1,7 @@
 #Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
-
-if(10 < 12):
-    print("É maior que 10")
+print("Digiter um valor inteiro: ")
+numero = int(input())
+if numero > 10:
+    print("Esse numero é maior que o 10")
 else:
-    print("Não é maior que 10")
+    print("Esse numero não é maior que o 10 ")
