cf71a9a444e1a0c7eeeefc3adc1e8de6ddea4468
jeancosta66 <costajean1005@gmail.com>
2025-03-23 22:13:00 -0300

    Update ex_006.py

--- ex_006.py
+++ ex_006.py
@@ -1,10 +1,10 @@
 #. Menu - Elabore um programa que mostre o seguinte menu na tela:
-Cadastro de Clientes
-0 - Fim
-1 - Inclui
-2 - Altera
-3 - Exclui
-4 - Consulta
+#Cadastro de Clientes
+#0 - Fim
+#1 - Inclui
+#2 - Altera
+#3 - Exclui
+#4 - Consulta
 
 cliente = input("Insirar seu nome: ")
 print("Digite um numero entre 0 até 4")
@@ -17,7 +17,7 @@ elif numero == 2:
     print("Alterar usuário")
 elif numero == 3:
     print("Excluir usuário")
-elif numero == 4:
+elif numero == 4: #elif: é junção do if + else. ele serve para coisas verdeiras ou falsas
     print("Consulta usuário")
-else:
+else: #else: nunca pode fica junto com uma variável
     print("Esse numero não custar no banco de dados")
