e6b7cc56a4586a5e26fede4ad089b6ebae8a914c
jeancosta66 <costajean1005@gmail.com>
2025-03-23 21:38:23 -0300

    Create ex_005.py

---
+++ ex_005.py
@@ -0,0 +1,21 @@
+#Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+
+#ALUNO(A)          NOTA
+#=========         =====
+#ALINE              9.0  
+#MÁRIO              DEZ
+#SÉRGIO             4.5    
+#SHIRLEY            7.0
+
+comando = "Aluno(a)           Nota"
+print(comando)
+comando = "=======           ======"
+print(comando)
+comando = "Aline              9.0"
+print(comando)
+comando = "Mário              DEZ"
+print(comando)
+comando = "Sérgio             4.5"
+print(comando)
+comando = "Shirley            7.0"
+print(comando)
