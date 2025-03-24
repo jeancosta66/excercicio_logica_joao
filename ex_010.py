c293b9021655ca276560a72e526bc5400387bf27
jeancosta66 <costajean1005@gmail.com>
2025-03-23 21:54:01 -0300

    Create ex_010.py

---
+++ ex_010.py
@@ -0,0 +1,15 @@
+#problema: escreva um software informe a média aritimética de um aluno que cursou 4 bimestres
+
+#Etapas para programar
+
+#1 - obter as 4 notas do aluno
+#2 - efetuar a conta
+#3 - mostrar a média aritimética
+
+notar1 = int(input("Digite a primeira notar (inteiro): "))
+notar2 = int(input("Digite o segunda notar (inteiro): "))
+notar3 = int(input("Digite a terceira notar (inteiro): "))
+notar4 = int(input("Digite a quarta notar (inteiro): "))
+
+s = (notar1 + notar2 + notar3 + notar4)/4
+print(f'A média aritimética dos valores {notar1}, {notar2}, {notar3},{notar4} vale: {s}')
