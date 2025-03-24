d16efcdd49b13d6c9e4994bbdf13aafa32edd115
jeancosta66 <costajean1005@gmail.com>
2025-03-23 22:11:18 -0300

    Create ex_011.py

---
+++ ex_011.py
@@ -0,0 +1,5 @@
+#Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor
+
+numero = float(input("Digite um valor qualquer: ")) #float: server para colocar numeros não inteiros
+antecessor = numero - 1 #serve para subtrair do numero 
+print("O antecessor do", numero , "é:" ,antecessor)   
