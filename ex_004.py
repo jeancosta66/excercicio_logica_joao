41543778ea7b26d13769a19ae8b05b35460efcbd
jeancosta66 <costajean1005@gmail.com>
2025-03-23 21:32:27 -0300

    Create ex_004.py

---
+++ ex_004.py
@@ -0,0 +1,18 @@
+#Quadrado - Escrever um programa que imprima a seguinte figura:
+
+#XXXXX
+#X   X
+#X   X
+#X   X
+#XXXXX
+
+comando = "xxxxx"
+print(comando)
+comando = "x   x"
+print(comando)
+comando = "x   x"
+print(comando)
+comando = "x   x"
+print(comando)
+comando = "xxxxx"
+print(comando)
