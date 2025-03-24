b734135b25735550c9f80404a47e8d45ca4ff1f8
jeancosta66 <costajean1005@gmail.com>
2025-03-23 22:16:12 -0300

    Update ex_011.py

--- ex_011.py
+++ ex_011.py
@@ -2,4 +2,4 @@
 
 numero = float(input("Digite um valor qualquer: ")) #float: server para colocar numeros não inteiros
 antecessor = numero - 1 #serve para subtrair do numero 
-print("O antecessor do", numero , "é:" ,antecessor)   
+print("O antecessor do", numero , "é:" ,antecessor)  #entre as virgulas serve para fazer uma interpolação de strings, invés do f 
