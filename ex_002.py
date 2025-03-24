f2b6725fe0ce4cf83ebe6b98566e7b0cbfb6be8e
jeancosta66 <costajean1005@gmail.com>
2025-03-23 21:30:38 -0300

    Create ex_002.py

---
+++ ex_002.py
@@ -0,0 +1,13 @@
+#Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+comando = "Nome?"
+print(comando)
+comando = input()
+
+comando = "estado?"
+print(comando)
+comando = input()
+
+comando = "Municípoio"
+print(comando)
+comando = input()
