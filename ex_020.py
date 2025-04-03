5b4131c1479e41772c4345540dcc42108409880e
jeancosta66 <costajean1005@gmail.com>
2025-04-03 20:54:54 -0300

    Create ex_020.py

---
+++ ex_020.py
@@ -0,0 +1,6 @@
+#Escreva um algoritmo para imprimir os números de 1 (inclusive) a 10 (inclusive) em ordem crescente
+for i in range(1, 11):
+    print(i)
+
+for i in range(10):
+    print("2mdsom", i)
