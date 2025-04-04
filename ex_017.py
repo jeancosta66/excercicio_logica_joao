2e7248580c125e61794ef8afd595faeec906fb2e
jeancosta66 <costajean1005@gmail.com>
2025-04-03 21:17:32 -0300

    Update ex_017.py

--- ex_017.py
+++ ex_017.py
@@ -14,3 +14,16 @@ elif n2 > n1 and n2 > n3:
     print("O segundo é maior")
 else:
     print("O terceiro é maior")
+--------------
+n1 = int(input("Informe o primeiro número: "))
+n2 = int(input("Informe o segundo número: "))
+while n2 == n1:
+    n2 = int(input("informe novamente"))
+n3 = int(input("Informe o terceiro número: "))
+
+if n1 > n2 and n1 > n3:
+    print("O primeiro é maior")
+elif n2 > n1 and n2 > n3:
+    print("O segundo é maior")
+else:
+    print("O terceiro é maior")
